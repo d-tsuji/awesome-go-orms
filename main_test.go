3ec6aaccb90043df42c344a9492444ed38842029
d-tsuji <dram.dt.shonan@gmail.com>
2020-04-19 08:52:05 +0900

    update test

--- main_test.go
+++ main_test.go
@@ -34,7 +34,7 @@ func Test_writeREADME(t *testing.T) {
 					},
 				},
 			},
-			wantW: `# Top Go ORMs
+			wantW: `# Top Go ORMs [![Go Report Card](https://goreportcard.com/badge/github.com/d-tsuji/awesome-go-orms)](https://goreportcard.com/report/github.com/d-tsuji/awesome-go-orms) [![Actions Status](https://github.com/d-tsuji/awesome-go-orms/workflows/CI/badge.svg)](https://github.com/d-tsuji/awesome-go-orms/actions)
 A list of popular github projects related to Go ORM(Object-Relational Mapping) (ranked by stars automatically)
 Please update **list.txt** (via Pull Request)
 
